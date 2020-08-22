307a348e5b5e7912f228c58d1b8767ce979317a1
titusringera <titusmbaabu@gmail.com>
2020-08-22 18:23:23 +0300

    second 1

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -5,20 +5,196 @@ import { Link } from 'react-router-dom';
 import { SidebarData } from './SidebarData';
 import './Navbar.css';
 import { IconContext } from 'react-icons';
+import {AppBar, Toolbar,Typography,Grid, InputBase, IconButton, Badge, makeStyles ,withStyles} from '@material-ui/core'
+import MenuItem from '@material-ui/core/MenuItem';
+import Menu from '@material-ui/core/Menu';
+import MenuIcon from '@material-ui/icons/Menu';
+import SearchIcon from '@material-ui/icons/Search';
+import AccountCircle from '@material-ui/icons/AccountCircle';
+import MailIcon from '@material-ui/icons/Mail';
+import NotificationsIcon from '@material-ui/icons/Notifications';
+import CreateDialog from './Contents/Dialogs/create';
+const useStyles = makeStyles(theme => ({
 
+  grow: {
+    flexGrow: 1,
+  },
+  appMain: {
+    display: 'flex',
+    flexDirection: 'column',
+    position: 'absolute',
+    left: '0px',
+    width: '250px',
+    height: '100%',
+    backgroundColor: '#3f51b5'
+  
+},
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    display: 'none',
+    [theme.breakpoints.up('sm')]: {
+      display: 'block',
+    },
+  },
+  searchInput: {
+      opacity: '1.6',
+      padding: `5px ${theme.spacing(1)}px`,
+      fontSize: '1.2rem',
+      //width:'400px',
+      color:'white',
+      '&:hover': {
+          backgroundColor: 'white',
+          color:'black'
+      },
+      '& .MuiSvgIcon-root': {
+          marginRight: theme.spacing(1)
+      },
+      borderRadius: 10
+  },
+  sectionDesktop: {
+    display: 'none',
+    [theme.breakpoints.up('md')]: {
+      display: 'flex',
+    },
+  },
+  sectionMobile: {
+    display: 'flex',
+    [theme.breakpoints.up('md')]: {
+      display: 'none',
+    },
+  },
+  appMain: {
+paddingLeft:'250px'
+  }
+  
+}))
 function Navbar() {
   const [sidebar, setSidebar] = useState(false);
 
   const showSidebar = () => setSidebar(!sidebar);
+  const classes = useStyles();
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const [mobileMoreAnchorEl, setMobileMoreAnchorEl] = React.useState(null);
 
+  const isMenuOpen = Boolean(anchorEl);
+  const isMobileMenuOpen = Boolean(mobileMoreAnchorEl);
+
+  const handleProfileMenuOpen = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleMobileMenuClose = () => {
+    setMobileMoreAnchorEl(null);
+  };
+
+  const handleMenuClose = () => {
+    setAnchorEl(null);
+    handleMobileMenuClose();
+  };
+
+  const handleMobileMenuOpen = (event) => {
+    setMobileMoreAnchorEl(event.currentTarget);
+  };
+
+  const menuId = 'primary-search-account-menu';
+  const renderMenu = (
+    <Menu
+      anchorEl={anchorEl}
+      anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
+      id={menuId}
+      keepMounted
+      transformOrigin={{ vertical: 'top', horizontal: 'right' }}
+      open={isMenuOpen}
+      onClose={handleMenuClose}
+    >
+      <MenuItem onClick={handleMenuClose}>Profile</MenuItem>
+      <MenuItem onClick={handleMenuClose}>My account</MenuItem>
+    </Menu>
+  );
+
+  const mobileMenuId = 'primary-search-account-menu-mobile';
+  const renderMobileMenu = (
+    <Menu
+      anchorEl={mobileMoreAnchorEl}
+      anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
+      id={mobileMenuId}
+      keepMounted
+      transformOrigin={{ vertical: 'top', horizontal: 'right' }}
+      open={isMobileMenuOpen}
+      onClose={handleMobileMenuClose}
+    >
+      <MenuItem>
+        <IconButton aria-label="show 4 new mails" color="inherit">
+          <Badge badgeContent={4} color="secondary">
+            <MailIcon />
+          </Badge>
+        </IconButton>
+        <p>Messages</p>
+      </MenuItem>
+      <MenuItem>
+        <IconButton aria-label="show 11 new notifications" color="inherit">
+          <Badge badgeContent={11} color="secondary">
+            <NotificationsIcon />
+          </Badge>
+        </IconButton>
+        <p>Notifications</p>
+      </MenuItem>
+      <MenuItem onClick={handleProfileMenuOpen}>
+        <IconButton
+          aria-label="account of current user"
+          aria-controls="primary-search-account-menu"
+          aria-haspopup="true"
+          color="inherit"
+        >
+          <AccountCircle />
+        </IconButton>
+        <p>Profile</p>
+      </MenuItem>
+    </Menu>
+  );
   return (
     <>
       <IconContext.Provider value={{ color: '#fff' }}>
-        <div className='navbar'>
+      
+          <AppBar position="static" 
+          //className={classes.appMain} 
+          >
+            <div className='navbar'>
           <Link to='#' className='menu-bars'>
-            <FaIcons.FaBars onClick={showSidebar} />
+            <FaIcons.FaBars onClick={showSidebar} 
+            />
           </Link>
-        </div>
+    <Toolbar>
+   
+          <Typography 
+          className={classes.title} 
+          variant="h6" noWrap>
+           MY PROJECT
+          </Typography>
+      <Grid container
+                      alignItems="center">
+                      <Grid item>
+                          <InputBase
+                              placeholder="Search Engineer"
+                              className={classes.searchInput}
+                              startAdornment={<SearchIcon fontSize="small" />}
+                          />
+                      </Grid>
+                      <Grid item sm></Grid>
+
+                      
+                  </Grid>
+      <Typography variant="headline"  style={{flex: 1}} >
+   
+    </Typography>
+    <CreateDialog/>
+   
+    </Toolbar>
+   
+   
+    </div>
         <nav className={sidebar ? 'nav-menu active' : 'nav-menu'}>
           <ul className='nav-menu-items' onClick={showSidebar}>
             <li className='navbar-toggle'>
@@ -37,7 +213,12 @@ function Navbar() {
               );
             })}
           </ul>
+         
+   
+   
         </nav>
+  </AppBar>
+        
       </IconContext.Provider>
     </>
   );
